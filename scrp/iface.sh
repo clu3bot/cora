1d23d85c8c71c45dcba5501a3e03f5bfc42a5518
Brennan Mccown <clu3bot@protonmail.com>
2022-01-26 22:53:15 -0500

    Update iface.sh

--- scrp/iface.sh
+++ scrp/iface.sh
@@ -15,52 +15,52 @@ var=1000
 
 end(){
         echo -e "\n${LBLUE}[${LGREEN}${int}${LBLUE}] Selected"
-        read -r -p "$(tput setaf 7)Press Enter to Continue.."
-        echo "$int" > tmp/var.txt
-        rm -rf tmp/wifiifaces.csv
+                read -r -p "$(tput setaf 7)Press Enter to Continue.."
+                echo "$int" > scrp/tmp/var.txt
+        rm -rf scrp/tmp/wifiifaces.csv
 
 }
 
 notvalid(){
         clear
         unset $int
-        echo -e "${LRED}Your selection was not valid"
-        int="An Error has occured with your Interface"
+                echo -e "${LRED}Your selection was not valid"
+                int="An Error has occured with your Interface"
         sleep 3
 end
 }
 
 valid(){
-        rm -rf tmp/var.txt
+        rm -rf scrp/tmp/var.txt
         clear
-        echo "${int}" > tmp/var.txt
-        sleep 0.5
+                echo "${int}" > scrp/tmp/var.txt
+                        sleep 0.5
 end
 }
 
 inputvalidation(){
         if echo $int | grep -E '[ "]' >/dev/null
         then
-        notvalid
+                notvalid
         else
-        valid
+                valid
         fi
 }
 
 output(){
         unset $int
-        rm -rf tmp/var.txt
-        clear
-        echo -e "${PURPLE}Select an Interface:\n${LBLUE}"
-        iwconfig 2>&1 | grep -oP "^\w+" > tmp/wifiifaces.csv
-        ##
-        cut -d "," -f 14 tmp/wifiifaces.csv | nl -n ln -w 6
-                while [ ${var} -gt "$(wc -l tmp/wifiifaces.csv | cut -d " " -f 1)" ] || [ ${var}} -lt 1 ]; do
-        echo -e "\nSelect a Network"
-        read -r -p "$(tput setaf 7)" var
-                done
-        int=$(sed -n "${var}p" < tmp/wifiifaces.csv | cut -d "," -f 14 )
-        rm -rf tmp/wifiifaces.csv
+                rm -rf scrp/tmp/var.txt
+                clear
+                         echo -e "${PURPLE}Select an Interface:\n${LBLUE}"
+                         iwconfig 2>&1 | grep -oP "^\w+" > scrp/tmp/wifiifaces.csv
+                         ##
+                                cut -d "," -f 14 scrp/tmp/wifiifaces.csv | nl -n ln -w 6
+                                         while [ ${var} -gt "$(wc -l scrp/tmp/wifiifaces.csv | cut -d " " -f 1)" ] || [ ${var}} -lt 1 ]; do
+                         echo -e "\nSelect a Network"
+                        read -r -p "$(tput setaf 7)" var
+                                         done
+                int=$(sed -n "${var}p" < scrp/tmp/wifiifaces.csv | cut -d "," -f 14 )
+                rm -rf scrp/tmp/wifiifaces.csv
         clear
 }
 output
