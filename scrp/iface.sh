2cdbeeab51e74f31baaeeaf21bc6bbc1b0fba006
Brennan Mccown <clu3bot@protonmail.com>
2022-01-30 11:25:30 -0500

    Update iface.sh

--- scrp/iface.sh
+++ scrp/iface.sh
@@ -1,57 +1,67 @@
-#!/bin/bash/
+#!/bin/bash
+#colors vars
+LBLUE='\033[1;34m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NONE='\033[0m'
+PURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+#
 
-check_iface() {
-    iface=$(cat scrp/tmp/int.txt)
-    echo $iface
-        if [[ -z "${iface// }" ]]; then
-            iface="eth0"
-        fi
+var=1000
 
-            if [[ "${iface}" == "eth0" ]]; then
-                mode="Wired Connection"
-            else
-                mode=$(iwconfig "$iface" | sed -n '/Mode:/s/.*Mode://; s/ .*//p')
-            fi
+end(){
+        echo -e "\n${NONE}[${LGREEN}${int}${NONE}] Selected"
+        read -r -p "$(tput setaf 7)Press Enter to Continue.."
+        echo "$int" > scrp/tmp/var.txt
+        rm -rf scrp/tmp/wifiifaces.csv
 
-        if [ "$mode" == "Monitor" ]; then 
-            Mod=Monitor 
-        else   
-            Mod=Managed       
-        fi
 }
 
-main(){
-clear
-    check_iface
-        if [ "$Mod" == "Monitor" ]; then
-            sleep 0.1
-        else
-            echo -e "${LRED}This tool requires Monitor Mode${NONE}"
-            read -r -p "Press Enter to return to Main Menu"
-                main_menu
-        fi
-    scan_animation &
-        #failsafe for exit
-        trap 'airmon-ng stop $iface > /dev/null;rm otp-01.csv 2> /dev/null' EXIT
-            xterm -e airodump-ng --output-format csv -w otp "$iface" > /dev/null & sleep 10 ; kill $!
-            sed -i '1d' otp-01.csv
-        kill %1
-    echo -e "\n\n${LRED}Scan Results${NONE}"
-        cut -d "," -f 14 otp-01.csv | nl -n ln -w 6
-            while [ ${S} -gt "$(wc -l otp-01.csv | cut -d " " -f 1)" ] || [ ${S} -lt 1 ]; do
-                echo -e "\n${LBLUE}Select a Network"
-                read -r -p "$(tput setaf 7) " S
-            done
-        nn=$(sed -n "${S}p" < otp-01.csv | cut -d "," -f 14 )
-        rm -rf otp-01.csv 2> /dev/null
-    echo -e "\n[${LGREEN}${nn}${NONE} ] Selected"
-    read -r -p "$(tput setaf 7)Press Enter to Continue.."
+notvalid(){
         clear
-        main_menu
+        unset $int
+                echo -e "${LRED}Your selection was not valid"
+                int="An Error has occured with your Interface"
+        sleep 3
+end
 }
 
-initial(){
-    main
+valid(){
+        rm -rf scrp/tmp/var.txt
+                clear
+                echo "${int}" > scrp/tmp/var.txt
+        sleep 0.5
+end
 }
 
-initial
+inputvalidation(){
+        if echo $int | grep -E '[ "]' >/dev/null
+        then
+                notvalid
+        else
+                valid
+        fi
+}
+
+output(){
+        unset $int
+                rm -rf scrp/tmp/var.txt
+                clear
+                        echo -e "Select an Interface:\n"
+                        iwconfig 2>&1 | grep -oP "^\w+" > scrp/tmp/wifiifaces.csv
+                        ##
+                                cut -d "," -f 14 scrp/tmp/wifiifaces.csv | nl -n ln -w 6
+                                while [ ${var} -gt "$(wc -l scrp/tmp/wifiifaces.csv | cut -d " " -f 1)" ] || [ ${var}} -lt 1 ]; do
+                        echo -e "\nSelect a Network"
+                        read -r -p "$(tput setaf 7)" var
+                                done
+                int=$(sed -n "${var}p" < scrp/tmp/wifiifaces.csv | cut -d "," -f 14 )
+                rm -rf scrp/tmp/wifiifaces.csv
+                clear
+}
+output
+inputvalidation
