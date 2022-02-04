88d57d89dea5f446c98997cdb34caf94fbf7eba8
Brennan Mccown <clu3bot@protonmail.com>
2022-02-04 12:36:33 -0500

    Update base-64.py

--- etc/crypto/base-64.py
+++ etc/crypto/base-64.py
@@ -1,9 +1,13 @@
 import base64
+import os
+import time
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#encode and decode with base64
 def initial():
-  
+    clear()
+
     userinput = input("Encode/Decode base64: ")
     inputencode = userinput.encode("ascii")
   
@@ -11,5 +15,33 @@ def initial():
     base64str = base64encode.decode("ascii")
   
     print("Encoded/Decoded string: "+base64str)
+    return base64str
 
 initial()
+
+def ask():
+
+    base64str = initial()
+    clear()
+    print (base64str)
+    userchoice = input ("Save this string to a file? y/n")
+
+    userchoicelower = userchoice.lower()
+    if userchoicelower == 'y':
+        os.system("echo "+base64str+" > backupstrs/base64str.txt")
+        print ("File saved to cora/etc/crypto/backupstrs/")
+    elif userchoicelower == 'n':
+        time.sleep(1)
+    else:
+        print ("Invalid Option..")
+        time.sleep(2)
+        ask()
+
+ask()
+
+def last():
+    clear()
+    input ("Press any key to return to Crypto Menu..")
+
+last()
+
