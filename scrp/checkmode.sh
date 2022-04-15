079c3e9e97dbd9b82b19098dc0fd37aa02f76dfe
Brennan Mccown <clu3bot@protonmail.com>
2022-04-14 21:07:33 -0400

    Update checkmode.sh

--- scrp/checkmode.sh
+++ scrp/checkmode.sh
@@ -2,5 +2,20 @@
 
 iface=$(airmon-ng | awk 'NR==4' | awk '{print $2}')
 
-echo $iface
+if [[ -z "${iface// }" ]]; then
+    iface="eth0"
+fi
+    
+if [[ "${iface}" == "eth0" ]]; then
+    mode="Wired Connection"
+else
+    mode=$(iwconfig "$iface" | sed -n '/Mode:/s/.*Mode://; s/ .*//p')
+fi
 
+if [ "$mode" == "Monitor" ]; then 
+    Mod=Monitor 
+else 
+    Mod=Managed 
+fi
+
+echo $Mod
