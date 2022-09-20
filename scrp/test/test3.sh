bd94edf3f134e8208da8606fd61fb79d4c1a0e17
Brennan Mccown <clu3bot@protonmail.com>
2022-09-19 22:07:01 -0400

    Delete scrp/test directory

--- scrp/test/test3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-interface=$(cat int.txt)
-
-x=$(iwconfig 2>&1 | grep -n $interface | awk '{ print $1 }' | cut -d: -f1)
-
-iwconfig 2>&1 | awk 'NR=='$x'' | awk '{ print $1 }'
--- scrp/test/test5.sh
+++
@@ -1,5 +0,0 @@
-x=$(cat int.txt | sed s/"mon"//)
-
-rm -rf int.txt
-
-echo $x > int.txt
