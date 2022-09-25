a11d2ce67f581755467e71209e53f4aeac6fc753
Brennan Mccown <clu3bot@protonmail.com>
2022-09-25 02:12:48 +0000

    Update networkselect.sh

--- scrp/networkselect.sh
+++ scrp/networkselect.sh
@@ -1,53 +1,28 @@
 #!/bin/bash/
 #var
 value=1000
-
-check_iface() {
-    iface=$(cat scrp/tmp/intexport.txt)
-
-        if [[ -z "${iface// }" ]]; then
-            iface="eth0"
-        fi
-            if [[ "${iface}" == "eth0" ]]; then
-                mode="Wired Connection"
-            else
-                mode=$(iwconfig "$iface" | sed -n '/Mode:/s/.*Mode://; s/ .*//p')
-            fi
-
-        if [ "$mode" == "Monitor" ]; then 
-            Mod=Monitor 
-        else   
-            Mod=Managed       
-        fi
-}
+int=$(sudo python3 scrp/inthandler/rename.py)
 
 command(){
-    airodump-ng --output-format csv -w otp "$iface" > /dev/null & sleep 10 ; kill $! 
+    airodump-ng --output-format csv -w otp "$int" > /dev/null & sleep 11 ; kill $! 
 }
 
 main(){
-clear
-    check_iface
-    if [ "$Mod" == "Monitor" ]; then
-        sleep 0.1
-    else
-        echo -e "${LRED}This tool requires Monitor Mode${NONE}"
-        read -r -p "Press Enter to return to Main Menu"
-        exit
-    fi
-        #failsafe for exit
-    trap 'airmon-ng stop $iface > /dev/null;rm otp-01.csv 2> /dev/null' EXIT
+clear       
+    #failsafe for exit
+    trap 'airmon-ng stop $int > /dev/null;rm otp-01.csv 2> /dev/null' EXIT
     command
-        sed -i '1d' otp-01.csv
+    sed -i '1d' otp-01.csv
     kill %1
+    clear
     echo -e "\n\n${LRED}Scan Results${NONE}"
-        cut -d "," -f 14 otp-01.csv | nl -n ln -w 6
-    while [ ${value} -gt "$(wc -l otp-01.csv | cut -d " " -f 1)" ] || [ ${value} -lt 1 ]; do
-        echo -e "\n${LBLUE}Select a Network"
-        read -r -p "$(tput setaf 7) " value
-    done
-        essid=$(sed -n "${value}p" < otp-01.csv | cut -d "," -f 14 )
-        rm -rf otp-01.csv 2> /dev/null
+    cut -d "," -f 14 otp-01.csv | nl -n ln -w 6
+        while [ ${value} -gt "$(wc -l otp-01.csv | cut -d " " -f 1)" ] || [ ${value} -lt 1 ]; do
+            echo -e "\n${LBLUE}Select a Network"
+            read -r -p "$(tput setaf 7) " value
+        done
+    essid=$(sed -n "${value}p" < otp-01.csv | cut -d "," -f 14 )
+    rm -rf otp-01.csv 2> /dev/null
     echo -e "\n[${LGREEN}${essid}${NONE} ] Selected"
     echo $essid > scrp/tmp/essid.txt
     read -r -p "$(tput setaf 7)Press Enter to Continue.."
